921e76c115351d161af18a8bbd8cccdbe1d93944
noInvazion <tonaleks222@gmail.com>
2023-02-18 12:26:57 +0000

    Last task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point to the function that is printing out a quote
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	putchar("and that piece of art is useful\" -Dora Korpar, 2015-10-19\n");
+	return (1);
+}
