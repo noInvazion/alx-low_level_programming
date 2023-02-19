668f12abe4af006e5b90f1b44c2d457c5413710b
noInvazion <tonaleks222@gmail.com>
2023-02-19 16:52:49 +0000

    Had some grammar mistakes

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,10 +9,12 @@
 int main(void)
 {
 	int n;
-	int last = n % 10;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
+	int last = n % 10;
+
 	if (n > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, last);
 	if (n == 0)
