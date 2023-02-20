e46ca5dc3571c0c8dae5547e40606689c6464fa1
noInvazion <tonaleks222@gmail.com>
2023-02-20 17:54:18 +0000

    Printing diggits from 0 to 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point: functions to print digits 0 to 10
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 10; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+
+}
