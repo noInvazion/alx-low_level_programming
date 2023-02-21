3411dd7eb359dee90960c98b0f882d9b2f35c236
noInvazion <tonaleks222@gmail.com>
2023-02-21 22:02:08 +0000

    Task six

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - it computes the abslute value of an integer
+ *
+ * @int: a datatype of some sort
+ * Return: the abs of the integer
+ */
+
+int _abs(int)
+{
+	int n;
+
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
+
