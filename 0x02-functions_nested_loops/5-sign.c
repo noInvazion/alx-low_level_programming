99457eecfa9a44cd41576c7d31d05638ffd0a27c
noInvazion <tonaleks222@gmail.com>
2023-02-21 21:08:12 +0000

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_sign - a function for numbers check
+ *
+ * Return: 0 if some condiitons are met, returns 0 when others are met
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		 _putchar(',');
+		 return (0);
+	}
+	else
+	{
+		_putchar('-');
+		_putchar(',');
+		return (-1);
+	}
+}
+	
