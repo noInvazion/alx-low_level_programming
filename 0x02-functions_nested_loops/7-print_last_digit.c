d3019ac0a59056c9eea115090e63921ee313e697
noInvazion <tonaleks222@gmail.com>
2023-02-21 22:49:27 +0000

    Task seven

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - returns the value of the last digit *
+ * @n: variable
+ *
+ * Return:  value of last digits!
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
