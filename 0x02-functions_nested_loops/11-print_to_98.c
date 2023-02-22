8911808a1a602035409f9062d10616adf4fc8a48
noInvazion <tonaleks222@gmail.com>
2023-02-22 21:45:55 +0000

    task eleven I

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_to_98 - function that prints all numbers from n to 98
+ * @n: variable for the starting point
+ */
+
+void print_to_98(int n)
+{
+	for (n = n; n <= 98; n++)
+	{
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+		if (n != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+}
