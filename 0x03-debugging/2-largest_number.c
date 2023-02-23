5240d25005f13c975f39980dec57d60403f1424e
noInvazion <tonaleks222@gmail.com>
2023-02-23 23:40:35 +0000

    Task onne

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -4,7 +4,7 @@
  * largest_number - returns the largest of 3 numbers
  * @a: first integer
  * @b: second integer
- * @c: third integer\
+ * @c: third integer
  * Return: 0
  */
 
@@ -16,7 +16,7 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (b > a $$ b > c)
+	else if (b > c)
 	{
 		largest = b;
 	}
