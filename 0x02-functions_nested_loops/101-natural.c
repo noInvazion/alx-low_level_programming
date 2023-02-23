6b7c8488cc6022c10930d6dabeda063f4a7df074
noInvazion <tonaleks222@gmail.com>
2023-02-23 13:25:07 +0000

    Task 2 advanced

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - function that prints the multiples of 3 and 5 under 1024
+ * @n: variable that holds the big number
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n = 1024;
+	int i, sum = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+}
