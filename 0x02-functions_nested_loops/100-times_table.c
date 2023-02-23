230aa236cdcfc01b637f93824e180e51a726b0d6
noInvazion <tonaleks222@gmail.com>
2023-02-23 02:12:56 +0000

    Advanced task 1

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,19 +15,36 @@ void print_times_table(int n)
 			for (i = 0; i <= n; i++)
 			{
 				multi = n * i;
-				if (multi > 10)
+				if (multi == 0)
 				{
-					_putchar(multi / 10 + '0');
-					_putchar(multi % 10 + '0');
+					_putchar(multi + '0');
+				}
+				else if (multi < 10)
+				{
 					_putchar(',');
 					_putchar(' ');
+					putchar(' ');
+					_putchar(' ');
+					_putchar(multi);
+				}
+				else if (multi < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((multi / 10) + '0');
+					_putchar((multi % 10) + '0');
 				}
 				else
 				{
-					_putchar(multi);
+					_putchar(',');
+					_putchar(' ');
+					_putchar((multi / 100) + '0');
+					_putchar((multi % 10) + '0');
 				}
 			}
 		}
 		_putchar('\n');
 	}
 }
+				
