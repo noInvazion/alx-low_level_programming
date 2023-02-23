3b26444723597c83e6dc7116e2bb6c7779017b56
noInvazion <tonaleks222@gmail.com>
2023-02-23 01:03:09 +0000

    task nine IX

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -24,7 +24,7 @@ void times_table(void)
 			{
 				_putchar(calc + '0');
 			}
-			if (i >= 9)
+			if (i <= 9)
 			{
 				_putchar(',');
 				_putchar(' ');
@@ -33,7 +33,6 @@ void times_table(void)
 			else if (i != 9)
 			{
 				_putchar(',');
-				_putchar(' ');
 			}
 		}
 		_putchar('\n');
