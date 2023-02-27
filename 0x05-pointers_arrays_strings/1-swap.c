226f5dd75e2fe311e3e7f57755e042b09a971ed4
noInvazion <tonaleks222@gmail.com>
2023-02-27 13:25:58 +0000

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - function that swaps two integer values
+ * @a: var for a
+ * @b: var for b
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = a;
+	a = b;
+	b = c;
+
+	return (0);
+}
