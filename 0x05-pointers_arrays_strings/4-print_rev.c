474920a4fc019ab4de97d8fbb48a2cd79208b30b
noInvazion <tonaleks222@gmail.com>
2023-02-27 15:53:53 +0000

    task 5

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,10 +12,10 @@ void print_rev(char *s)
 	while (s[n] != '\0')
 	{
 		n++;
-		for (i = n - 1; i >= 0; i--)
-		{
-			_putchar(s[i]);
-		}
+	}
+	for (i = n - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
 	}
 	_putchar('\n');
 }
