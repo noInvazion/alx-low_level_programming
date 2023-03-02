af44957d850efdd58f2b574506f950b203ce0b63
noInvazion <tonaleks222@gmail.com>
2023-03-02 17:51:45 +0000

    Task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * *_strncat - function that concatenates
+ * @dest: pointer variable for dest
+ * @src: pointer variable for src
+ * @n: variable of int data type
+ * Return: pointer to dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *str = dest;
+	int j;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	for (j = 0; j < n; j++)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+
+	*dest = '\0';
+
+	return (str);
+}
