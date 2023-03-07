b38e34de10e57e653130e3f328f055ebd9693e75
noInvazion <tonaleks222@gmail.com>
2023-03-07 10:18:13 +0000

    Task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+include "main.h"
+
+/**
+ * _memcpy - function that copies bytes from src to dest
+ * @dest: pointer variable that points to char
+ * @src: pointer variable that points to char
+ * @n: int datatype for n
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return dest;
