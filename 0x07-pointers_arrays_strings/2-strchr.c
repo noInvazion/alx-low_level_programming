b6c59f748dd2d0bc18462e625f7c8c72900010e2
noInvazion <tonaleks222@gmail.com>
2023-03-07 17:38:31 +0000

    Task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,15 +8,19 @@
 
 char *_strchr(char *s, char c)
 {
-	char *p = s;
-
-	while (*p != '\0')
+	while (*s != '\0')
 	{
-		if (*p == c)
+		if (*s == c)
 		{
-			return p;
+			return (s);
 		}
-		p++;
+		s++;
 	}
+
+	if (*s == c)
+	{
+		return (s);
+	}
+
 	return (NULL);
 }
