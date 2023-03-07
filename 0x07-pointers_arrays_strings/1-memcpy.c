837bcca45888475f05c2a9754b0ef9e40e5d70a4
noInvazion <tonaleks222@gmail.com>
2023-03-07 10:21:03 +0000

    Task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,13 +5,15 @@ include "main.h"
  * @dest: pointer variable that points to char
  * @src: pointer variable that points to char
  * @n: int datatype for n
+ * Return: dest
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
+
 	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
 	}
-	return dest;
+	return (dest);
