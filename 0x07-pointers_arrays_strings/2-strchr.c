c135f7a160bcbe75fc414fcffa95b73d3df8db68
noInvazion <tonaleks222@gmail.com>
2023-03-07 11:09:23 +0000

    Task 2:

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strchr - function that locates a character in a string
+ * @s: pointer variable to char datatype
+ * @c: pointer variable to char datatype
+ * Return: first occurence of c or NULL if none is found
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+	}
+
+	return (NULL);
+}
