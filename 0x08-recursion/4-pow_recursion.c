b26688b0bb03d5b5418269478e2e5d5cdee46ab7
noInvazion <tonaleks222@gmail.com>
2023-03-09 02:43:22 +0000

    Task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * _pow_recursion - function that returns the value of x raised to the power of y
+ * _pow_recursion - function that returns the value of x raised to y
  * @x: base value
  * @y: exponent value
  * Return: the result of x raised to ppower y
- * noInvazion
  */
-
 int _pow_recursion(int x, int y)
 {
+	if (y == 0)
+		return (1);
 	if (y < 0)
 		return (-1);
 	return (x * _pow_recursion(x, y - 1));
