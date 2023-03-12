6de68317b4bdd5c1c918d2d3da535437e72e73ab
noInvazion <tonaleks222@gmail.com>
2023-03-12 15:45:16 +0000

    Task 0

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - function that returns the square root of a number
+ * @n: variable n of integer datatype
+ * Return: natural square root of n
+ * noInvazion
+ */
+
+int _sqrt_recursion(int n)
+{
+	
