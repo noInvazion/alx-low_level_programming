1cafc226113008c749120aba44aa5989d4bd9771
noInvazion <tonaleks222@gmail.com>
2023-03-16 11:24:31 +0000

    Task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 	{
 		int num1 = atoi(argv[1]);
 		int num2 = atoi(argv[2]);
-		int res = num1 + num2;
+		int res = num1 * num2;
 
 		printf("%d\n", res);
 		return (0);
