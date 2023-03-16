83aaba0677b6f0dfc214a84fbf8c652aaf10c9d0
noInvazion <tonaleks222@gmail.com>
2023-03-16 10:57:36 +0000

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the numbers of args
+ * @argc: holds number of arguments passed
+ * @argv: holds the actual elements passed
+ * Return: 0
+ * noInvazion
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
+	int res = num1 + num2;
+
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", res);
+		return (0);
+	}
+	return(0);
+}
