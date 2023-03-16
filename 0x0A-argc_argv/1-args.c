535b04c1a6b1a6b1bc305d54e81875fe85ba16c6
noInvazion <tonaleks222@gmail.com>
2023-03-16 09:43:42 +0000

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - prints the numbers of args
+ * @argc: holds number of arguments passed
+ * @argv: holds the actual elements passed
+ * Return: 0
+ * noInvazion
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
